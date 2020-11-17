26cfcfe13d237a9f91de9c17b203e32e63cebb9d
Denis Pushkarev <zloirock@zloirock.ru>
2020-11-18 05:44:34 +0700

    remove `esnext.global-this` (moved to stable es)

--- packages/core-js-compat/src/data.js
+++ packages/core-js-compat/src/data.js
@@ -1300,8 +1300,6 @@ const data = {
   },
   'esnext.composite-symbol': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.global-this': null,
   'esnext.iterator.constructor': {
   },
   'esnext.iterator.as-indexed-pairs': {
@@ -1568,7 +1566,6 @@ const data = {
 
 // TODO: Remove from `core-js@4`
 data['esnext.aggregate-error'] = data['es.aggregate-error'];
-data['esnext.global-this'] = data['es.global-this'];
 data['esnext.promise.all-settled'] = data['es.promise.all-settled'];
 data['esnext.promise.any'] = data['es.promise.any'];
 data['esnext.string.match-all'] = data['es.string.match-all'];
