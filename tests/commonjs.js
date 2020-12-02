3db25a38ebd9674da5c65d9433e16ccc8a189da4
Denis Pushkarev <zloirock@zloirock.ru>
2020-12-02 19:47:08 +0700

    drop `es.date.now`

--- tests/commonjs.js
+++ tests/commonjs.js
@@ -274,7 +274,6 @@ for (const _PATH of ['../packages/core-js-pure', '../packages/core-js']) {
   ok(load('features/json').stringify([1]) === '[1]');
   ok(load('features/json/stringify')([1]) === '[1]');
   ok(load('features/json/to-string-tag') === 'JSON');
-  ok(typeof load('features/date/now')(new Date()) === 'number');
   ok(typeof load('features/date/to-string')(new Date()) === 'string');
   ok(typeof load('features/date/to-primitive')(new Date(), 'number') === 'number');
   ok(typeof load('features/date/to-iso-string')(new Date()) === 'string');
@@ -632,7 +631,6 @@ for (const _PATH of ['../packages/core-js-pure', '../packages/core-js']) {
   ok(load('stable/json').stringify([1]) === '[1]');
   ok(load('stable/json/stringify')([1]) === '[1]');
   ok(load('stable/json/to-string-tag') === 'JSON');
-  ok(typeof load('stable/date/now')(new Date()) === 'number');
   ok(typeof load('stable/date/to-string')(new Date()) === 'string');
   ok(typeof load('stable/date/to-primitive')(new Date(), 'number') === 'number');
   ok(typeof load('stable/date/to-iso-string')(new Date()) === 'string');
@@ -904,7 +902,6 @@ for (const _PATH of ['../packages/core-js-pure', '../packages/core-js']) {
   ok(load('es/json').stringify([1]) === '[1]');
   ok(load('es/json/stringify')([1]) === '[1]');
   ok(load('es/json/to-string-tag') === 'JSON');
-  ok(typeof load('es/date/now')(new Date()) === 'number');
   ok(typeof load('es/date/to-string')(new Date()) === 'string');
   ok(typeof load('es/date/to-primitive')(new Date(), 'number') === 'number');
   ok(typeof load('es/date/to-iso-string')(new Date()) === 'string');
